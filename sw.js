82a7c79f6894dcd6d809aa819526b65ea276bf9f
汪汪 <132054009+KegangWangCCNU@users.noreply.github.com>
2025-12-24 12:45:05 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'facephys-v1';
+
+const CORE_ASSETS = [
+    './',
+    './index.html',
+    './main.js',
+    './inference_worker.js',
+    './psd_worker.js',
+    './plot_worker.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CORE_ASSETS);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    return
+    if (!e.request.url.startsWith('http')) {
+        return; 
+    }
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(e.request).then((response) => {
+                if (!response || response.status !== 200 || response.type !== 'basic' && response.type !== 'cors') {
+                    return response;
+                }
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(e.request, responseToCache);
+                });
+                return response;
+            });
+        })
+    );
+});
